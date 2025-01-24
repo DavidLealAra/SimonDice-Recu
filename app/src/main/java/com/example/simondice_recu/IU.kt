0d9f53122f06d5b5ff6ea46b102d063b6f8887fc
DavidLealAra <dlealaraujo@danielcastelao.org>
2025-01-24 19:46:41 +0100

    Creo la clase IU

---
+++ app/src/main/java/com/example/simondice_recu/IU.kt
@@ -0,0 +1,11 @@
+package com.example.simondice_recu
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun IU() {
+    Column {
+
+    }
+}
