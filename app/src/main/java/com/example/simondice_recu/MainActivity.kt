56c62c1290f245a10ae86b4627a12ac76bed42b7
DavidLealAra <dlealaraujo@danielcastelao.org>
2025-01-24 19:46:06 +0100

    Creo la clase MainActivity

---
+++ app/src/main/java/com/example/simondice_recu/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.simondice_recu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import com.example.simondice_recu.ui.theme.SimonDiceRecuTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimonDiceRecuTheme {
+                Surface(
+                    color = Color.LightGray,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    IU(viewModel = ModelView(this))
+                }
+            }
+        }
+    }
+}
