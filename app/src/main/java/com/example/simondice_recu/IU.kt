2aa974b3e69c1e97697de5df72594c249a1cf97c
DavidLealAra <dlealaraujo@danielcastelao.org>
2025-01-24 20:14:53 +0100

    Añado estados, rondas y mensajes

--- app/src/main/java/com/example/simondice_recu/IU.kt
+++ app/src/main/java/com/example/simondice_recu/IU.kt
@@ -1,10 +1,27 @@
 package com.example.simondice_recu
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun IU() {
+fun IU(viewModel: ModelView) {
+    val estado by viewModel.estadoLiveData.observeAsState(Datos.Estados.INICIO)
+    val ronda by viewModel.rondaLiveData.observeAsState(0)
+    val mensaje by viewModel.mensajeLiveData.observeAsState("")
     Column {
 
     }
--- app/src/main/java/com/example/simondice_recu/ModelView.kt
+++ app/src/main/java/com/example/simondice_recu/ModelView.kt
@@ -1,4 +1,26 @@
-package com.example.simondice_recu;
+package com.example.simondice_recu
 
-public class ModelView {
+import android.media.MediaPlayer
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel que gestiona el estado y la lógica del juego "Simón Dice".
+ */
+class ModelView(private val activity: ComponentActivity) : ViewModel() {
+
+    // LiveData para el estado del juego
+    val estadoLiveData = MutableLiveData(Datos.Estados.INICIO)
+
+    // LiveData para la ronda actual
+    val rondaLiveData = MutableLiveData(0)
+
+    // LiveData para gestionar los mensajes
+    val mensajeLiveData = MutableLiveData<String>()
 }
