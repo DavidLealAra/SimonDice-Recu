bd8fc2d29b79beebe1be97405d195df22a62be1d
DavidLealAra <dlealaraujo@danielcastelao.org>
2025-01-24 20:02:58 +0100

    Subo la enum class de estados

--- app/src/main/java/com/example/simondice_recu/Datos.kt
+++ app/src/main/java/com/example/simondice_recu/Datos.kt
@@ -1,4 +1,15 @@
 package com.example.simondice_recu
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Color
+
 object Datos {
+
+    // Estados posibles del juego
+    enum class Estados(val value: Int, val label: String) {
+        INICIO(0, "Inicio"),
+        GENERANDO(1, "Generando"),
+        ADIVINANDO(2, "Adivinando"),
+        PERDIDO(3, "Perdido")
+    }
 }
